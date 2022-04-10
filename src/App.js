a3d79434138e717e108bc37fbbde9b8244b9c845
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-10 15:24:42 +0600

    basic form tested, react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import app, { getAuth } from './firebase.init';
+import { getAuth } from "firebase/auth";
+import app from './firebase.init';
 
 function App() {
   const auth = getAuth(app);
+
+  const handleEmailBlur = (e) =>{
+      console.log(e.target.value);
+  }
+  const handlePasswordBlur = (e) =>{
+      console.log(e.target.value);
+  }
+  const handleFormSubmit = (e) =>{
+      console.log('Form Submitted');
+      e.preventDefault();
+  }
   return (
     <div className="App">
-      
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmailBlur} type="email" name="" id="" />
+        <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 }
